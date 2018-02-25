9610f00daf6e408bb643e8d63c02e972d7c9af82
Evan Lloyd <evan.u.lloyd@gmail.com>
2018-02-25 10:06:43 -0500

    Add home screen logo

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -3,15 +3,18 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 import { View, StatusBar } from 'react-native';
 
 import { Container } from '../components/Container';
+import { Logo } from '../components/Logo';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
+  $white: '#FFFFFF',
 })
 
 const Home = () => (
   <Container>
     <StatusBar translucent={false} barStyle="light-content" />
     <View />
+    <Logo />
   </Container>
 )
 
